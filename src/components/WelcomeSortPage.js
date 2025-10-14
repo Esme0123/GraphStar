237359783b19e1d00145ef93b462ac94c9048df0
Esmeralda Medina <esmeralda.medina.p@ucb.edu.bo>
2025-10-14 18:13:25 -0400

    Sort Page imgs

--- src/components/WelcomeSortPage.js
+++ src/components/WelcomeSortPage.js
@@ -8,28 +8,28 @@ const constellationsData = [
       name: 'Andrómeda', 
       concept: 'Insertion Sort', 
       info: 'Funciona construyendo una secuencia ordenada un elemento a la vez. Es como ordenar una mano de cartas: tomas una carta y la insertas en su posición correcta entre las que ya tienes ordenadas.',
-      img: process.env.PUBLIC_URL + 'IMG/andromeda.jpg',
+      img: process.env.PUBLIC_URL + 'IMG/constelations/andromeda.jpg',
     },
     { 
       id: 'orion', 
       name: 'Orión', 
       concept: 'Selection Sort', 
       info: 'Busca repetidamente el elemento más pequeño de la parte no ordenada de la lista y lo mueve al principio. Es como si buscaras al estudiante más bajo de una fila y lo pusieras al frente, una y otra vez.',
-      img: process.env.PUBLIC_URL + 'IMG/orion.jpg',
+      img: process.env.PUBLIC_URL + 'IMG/constelations/orion.jpg',
     },
     { 
       id: 'gemini', 
       name: 'Géminis', 
       concept: 'Merge Sort', 
       info: 'Es un algoritmo de "divide y vencerás". Divide la lista a la mitad repetidamente hasta tener listas de un solo elemento. Luego, las combina (merge) de forma ordenada hasta tener una sola lista final.',
-      img: process.env.PUBLIC_URL + 'IMG/geminis.jpg',
+      img: process.env.PUBLIC_URL + 'IMG/constelations/geminis.jpg',
     },
     { 
       id: 'pegasus', 
       name: 'Pegaso', 
       concept: 'Shell Sort', 
       info: 'Es una versión mejorada del Insertion Sort. Permite el intercambio de elementos que están lejos uno del otro. Ordena sub-listas de elementos a intervalos decrecientes, haciéndolo más rápido en muchos casos.',
-      img: process.env.PUBLIC_URL + 'IMG/pegaso.jpg',
+      img: process.env.PUBLIC_URL + 'IMG/constelations/pegaso.jpg',
     }
 ];
 
@@ -47,7 +47,7 @@ const WelcomeSortPage = ({ onGoToSimulator, onGoBack }) => {
                 <div className="constellations-grid">
                     {constellationsData.map(item => (
                         <div key={item.id} className="constellation-item" onClick={() => setActiveConstellation(item)}>
-                            <img src={item.imageUrl} alt={item.name} className="constellation-image" />
+                            <img src={item.img} alt={item.name} className="constellation-image" />
                             <span className="constellation-name">{item.name}</span>
                             <span className="constellation-concept">{item.concept}</span>
                         </div>
@@ -59,7 +59,6 @@ const WelcomeSortPage = ({ onGoToSimulator, onGoBack }) => {
                 <button onClick={onGoBack}>Retroceder</button>
             </div>
 
-            {/* Pasamos los datos al InfoModal, que ya sabe cómo mostrar 'name' e 'info' */}
             <InfoModal planet={activeConstellation} onClose={() => setActiveConstellation(null)} />
         </div>
     );
